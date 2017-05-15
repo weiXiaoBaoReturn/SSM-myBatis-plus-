7e46fb337fd78823c86dd3f7213c1b257e1680c5
weiXiaoBaoReturn <wbb0116@163.com>
2017-05-15 11:09:20 +0800

    Update SysOfficeService.java

--- src/main/java/com/my/ssm/service/SysOfficeService.java
+++ src/main/java/com/my/ssm/service/SysOfficeService.java
@@ -1,3 +1,4 @@
+
 import com.my.ssm.entity.SysOffice;
 
 import java.util.List;
@@ -6,7 +7,10 @@ import java.util.List;
  * Created by ${wb} on 2017/4/26.
  */
 public interface SysOfficeService {
+    //获取所有单位
     List<SysOffice> getOffice();
+    //根据myBatis-plus的BaseMApper获取
     List<SysOffice> getOfficeByBaseMapper();
+    //根据名称获取单位
     SysOffice selectOfficeByName(String name);
 }
