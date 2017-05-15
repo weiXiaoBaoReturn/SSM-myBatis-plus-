9a52e741c0f247436ece48f9d6cfb136c061bf58
weiXiaoBaoReturn <wbb0116@163.com>
2017-05-15 11:10:36 +0800

    Update SysOfficeImpl.java

--- src/main/java/com/my/ssm/impl/SysOfficeImpl.java
+++ src/main/java/com/my/ssm/impl/SysOfficeImpl.java
@@ -16,8 +16,18 @@ import java.util.List;
 public class SysOfficeImpl implements SysOfficeService {
     @Resource
     private SysOfficeDao sysOfficeDao;
+    
     @Override
     public List<SysOffice> getOffice() {
         return sysOfficeDao.selectOffice();
+    }
+     @Override
+    public List<SysOffice> getOfficeByBaseMapper() {
+        return sysOfficeDao.selectList(
+                new EntityWrapper<SysOffice>().eq("name", "北京"));
+    }
+    @Override
+    public SysOffice selectOfficeByName(String name) {
+        return sysOfficeDao.selectOfficeByName(name);
     }
 }
