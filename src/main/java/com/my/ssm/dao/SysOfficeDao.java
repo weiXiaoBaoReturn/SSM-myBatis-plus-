66b1d096193b68fb3d488326702abcdb0a6cb779
weiXiaoBaoReturn <wbb0116@163.com>
2017-05-15 11:11:58 +0800

    Update SysOfficeDao.java

--- src/main/java/com/my/ssm/dao/SysOfficeDao.java
+++ src/main/java/com/my/ssm/dao/SysOfficeDao.java
@@ -1,6 +1,8 @@
 package com.my.ssm.dao;
 
+import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.my.ssm.entity.SysOffice;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
  * Created by ${wb} on 2017/4/26.
  */
 @Component
-public interface SysOfficeDao {
-    public List<SysOffice> selectOffice();
+public interface SysOfficeDao  extends BaseMapper<SysOffice>{
+    List<SysOffice> selectOffice();
+    //根据名称查询单位
+    SysOffice selectOfficeByName(@Param(value = "name")String name);
 }
